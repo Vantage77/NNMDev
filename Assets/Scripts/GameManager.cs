aafcca3b2dbec60d5e9d2aaac49f4a8245c49cdf
Vantage77 <jwiebe777@gmail.com>
2020-08-19 13:30:12 -0500

    Coin and score

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,23 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager instance;
+
+    public int currentScore = 0;
+
+    public Text score;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        instance = this;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        score.text = currentScore + "";
     }
+
+    public void AddScore()
+    {
+        currentScore++;
+    }
+
     public void RestartGame()
     {
         SceneManager.LoadSceneAsync(0);
     }
-
 }
--- Assets/Scripts/PlatformController.cs
+++ Assets/Scripts/PlatformController.cs
@@ -10,10 +10,16 @@ public class PlatformController : MonoBehaviour
     public int moveVel = 5;
 
     Transform startPosition;
+
+    public Transform[] coinSpawnPositions;
+
+    public GameObject coinPrefab;
+
+    GameObject spawnedCoin;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -21,12 +27,29 @@ public class PlatformController : MonoBehaviour
     {
         if (transform.position.x != endPosition.position.x)
         {
-            transform.position = Vector3.MoveTowards(transform.position,  new Vector3 (endPosition.position.x, transform.position.y, transform.position.z), Time.deltaTime * moveVel);
+            transform.position = Vector3.MoveTowards(transform.position, new Vector3(endPosition.position.x, transform.position.y, transform.position.z), Time.deltaTime * moveVel);
         }
         else
         {
+            if (spawnedCoin != null)
+            {
+                Destroy(spawnedCoin);
+            }
             startPosition = spawnPositions[Random.Range(0, spawnPositions.Length)];
             transform.position = startPosition.position;
+
+            int coin = Random.Range(0, 3);
+            Debug.Log("The coin is" + coin);
+            switch (coin)
+            {
+                case 0:
+                case 1:
+                    spawnedCoin = Instantiate(coinPrefab, coinSpawnPositions[coin].position, Quaternion.identity, transform);
+                    break;
+                case 2:
+                default:
+                    break;
+            }
         }
     }
 }
